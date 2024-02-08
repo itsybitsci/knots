ca379343a9284b4ac1f903df80dd7a75b0c54a8c
James Marnie Argarin <jnargarin@gmail.com>
2024-02-09 03:20:37 +0800

    Changed icons

--- src/components/shared/topbar.tsx
+++ src/components/shared/topbar.tsx
@@ -6,9 +6,9 @@ export default function Topbar() {
 	return (
 		<nav className='topbar'>
 			<Link href='/' className='flex items-center gap-4'>
-				<Image src='/assets/logo.svg' alt='logo' width={28} height={28} />
+				<Image src='/assets/knots_white.svg' alt='logo' width={28} height={28} />
 				<p className='text-heading3-bold text-light-1 max-xs:hidden'>
-					Threads
+					Knots
 				</p>
 			</Link>
 
