f96691214ca083f4ec513dd610e3626e2e735a15
James Marnie Argarin <jnargarin@gmail.com>
2024-02-09 18:21:59 +0800

    Remove loading states

--- src/app/(root)/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Icons } from '@/components/shared/icons'
-
-export default function Loading() {
-	return (
-		<div className='flex items-center justify-center w-full h-full'>
-			<Icons.loading className='h-11 w-11' />
-		</div>
-	)
-}
